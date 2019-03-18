666d37c578e2f53c7f4cdeb49d5bfcb5e264e80d
Anniken Holst <annikekh@stud.ntnu.no>
2019-03-18 15:32:09 +0100

    flask blueprints

--- manage.py
+++ manage.py
@@ -1,19 +1,17 @@
-from flask_script import Manager
+# manage.py
 
-from project import app, db
 
 import unittest
 
+from flask_script import Manager
 
+from project import create_app, db
+from project.api.models import User
+
+
+app = create_app()
 manager = Manager(app)
 
-
-@manager.command
-def recreate_db():
-    """Recreates a database."""
-    db.drop_all()
-    db.create_all()
-    db.session.commit()
 
 @manager.command
 def test():
@@ -23,6 +21,15 @@ def test():
     if result.wasSuccessful():
         return 0
     return 1
+
+
+@manager.command
+def recreate_db():
+    """Recreates a database."""
+    db.drop_all()
+    db.create_all()
+    db.session.commit()
+    
 
 if __name__ == '__main__':
     manager.run()
