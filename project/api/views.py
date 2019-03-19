892cd910fc68f4b12a398fa9630084e56832f7ff
Anniken Holst <annikekh@stud.ntnu.no>
2019-03-19 10:53:07 +0100

    Finish module 1 of tutorial

--- project/api/views.py
+++ project/api/views.py
@@ -1,7 +1,7 @@
 # project/api/views.py
 
 
-from flask import Blueprint, jsonify, request
+from flask import Blueprint, jsonify, request, render_template
 
 from project.api.models import User
 from project import db
@@ -9,7 +9,7 @@ from project import db
 from sqlalchemy import exc
 
 
-users_blueprint = Blueprint('users', __name__)
+users_blueprint = Blueprint('users', __name__, template_folder='./templates')
 
 
 @users_blueprint.route('/ping', methods=['GET'])
@@ -101,3 +101,14 @@ def get_all_users():
         }
     }
     return jsonify(response_object), 200
+
+
+@users_blueprint.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        username = request.form['username']
+        email = request.form['email']
+        db.session.add(User(username=username, email=email))
+        db.session.commit()
+    users = User.query.order_by(User.created_at.desc()).all()
+    return render_template('index.html', users=users)
